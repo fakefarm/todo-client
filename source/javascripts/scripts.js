4e3b744ddb40377fe9b8d4b773b369d10d219dc7
Dave Woodall <dave.woodall@livingsocial.com>
2015-02-28 12:07:10 -0700

    First style guide component

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,3 +1,4 @@
 //= require 'angular/angular.js'
 //= require 'app'
 //= require 'styleguide/styleguide_directives'
+//= require 'classie'
--- source/javascripts/styleguide/styleguide_directives.js
+++ source/javascripts/styleguide/styleguide_directives.js
@@ -1,7 +1,7 @@
 var app = angular.module('toDoApp')
 
-app.directive('datEnterTask', function() {
+app.directive('tdEnterTask', function() {
   return {
-    template: '<h1>yay!</h1>'
+    template: "<div class='container'><section class='content bgcolor-5'> <h2>Yoko</h2> <span class='input input--minoru'> <input class='input__field input__field--yoko' type='text' id='input-16' /> <label class='input__label input__label--yoko' for='input-16'> <span class='input__label-content input__label-content--yoko'>Street</span> </label> </span> <span class='input input--minoru'> <input class='input__field input__field--yoko' type='text' id='input-17' /> <label class='input__label input__label--yoko' for='input-17'> <span class='input__label-content input__label-content--yoko'>City</span> </label> </span> <span class='input input--minoru'> <input class='input__field input__field--yoko' type='text' id='input-18' /> <label class='input__label input__label--yoko' for='input-18'> <span class='input__label-content input__label-content--yoko'>Region</span> </label> </span> </section></div>"
   };
 });
